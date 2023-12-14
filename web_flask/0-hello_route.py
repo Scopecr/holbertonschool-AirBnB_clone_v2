d6c992a0524c78de75615fa40d642a48f3d111f2
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-14 09:12:47 -0400

    added framework

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/user/bin/python3
+'''
+starts a Flask web application
+'''
+
+from Flask import Flask
+app = flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+        """returns Hello HBNB!"""
+        return 'Hello HBNB!'
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+starts a Flask application
+"""
+
+from flask import Flask
+app = flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return Hello HBNB!"""
+    return  'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slahes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C' + text.replace('_',' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<test>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display "Pyython", followed by the calue of the text variable"""
+    return 'Pyython' + text.replace('_',' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
