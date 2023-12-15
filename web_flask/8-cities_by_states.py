7eb9f9989988fbbb13021e545727bd710b9e7ce2
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-15 18:37:42 -0400

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
 """
+Script that starts a Flask web application.
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Route taht displays the html page of
+    a list of states objects sorted by name
     """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    state_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=state_li)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Route that display a HTML page with a list of cities
+    objects sorted by name """
+    city_li = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=city_li)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_appcontext(exception):
+    """After each request, remove the SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
