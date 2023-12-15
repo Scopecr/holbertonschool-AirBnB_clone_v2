37fba3d968b14dc3a106717654abf21d82ae332d
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-15 11:29:05 -0400

    re organized 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,35 +9,47 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
-    return 'Hello HBNB!'
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
-    return 'HBNB'
+    """display text"""
+    return "HBNB"
 
 
 @app.route('/c/<text>')
-def c(text):
-    return f"C {text.replace('_', ' ')}"
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python')
 @app.route('/python/<text>')
-def python(text='is cool'):
-    return f"Python {text.replace('_', ' ')}"
+def python_text(text="is cool"):
+    """display custom text given
+       first route statement ensures it works for:
+          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
+          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
+    """
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>')
-def number(n):
-    return f"{n} is a number"
+def text_if_int(n):
+    """display text only if int given"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>')
-def number_template(n):
+def html_if_int(n):
+    """display html page only if int given
+       place given int into html template
+    """
     return render_template('5-number.html', n=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
