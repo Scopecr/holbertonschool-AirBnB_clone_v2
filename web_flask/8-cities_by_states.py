544838c7cc4297919e401d7d27edfcc335d908ec
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-15 17:25:42 -0400

    removed some functions

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,91 +1,24 @@
 #!/usr/bin/python3
 """ This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+""" Flask class and render_template method"""""
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """display text"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb')
-def hbnb():
-    """display text"""
-    return "HBNB"
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    """display custom text given"""
-    return "C {}".format(text.replace('_', ' '))
-
-
-@app.route('/python')
-@app.route('/python/<text>')
-def python_text(text="is cool"):
-    """display custom text given
-       first route statement ensures it works for:
-          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
-          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
-    """
-    return "Python {}".format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def text_if_int(n):
-    """display text only if int given"""
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>')
-def html_if_int(n):
-    """display html page only if int given
-       place given int into html template
-    """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def html_odd_or_even(n):
-    """display html page only if int given
-       place given int into html template
-       substitute text to display if int is odd or even
-    """
-    odd_or_even = "even" if (n % 2 == 0) else "odd"
-    return render_template('6-number_odd_or_even.html',
-                           n=n, odd_or_even=odd_or_even)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """after each request remove current SQLAlchemy session"""
+def teardown(self):
     storage.close()
-
-
-@app.route('/states_list')
-def states_list():
-    """display html page
-       fetch sorted states to insert into html in UL tag
-    """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/cities_by_states')
 def cities_by_states():
-    """display a HTML page with list of all State objects and their cities"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    for state in states:
-        state.cities = sorted(state.cities, key=lambda city: city.name)
+    states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
