bc73f1d5ae0af5ffc805709c318a330190f62345
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-15 18:48:53 -0400

    9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
-
-from models import storage
+""" This module starts a Flask web application """""
 from flask import Flask
 from flask import render_template
-
+from models import storage
+from models.state import State
+""" Flask class and render_template method"""""
 app = Flask(__name__)
-
-
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown():
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
     storage.close()
 
-if __name__ == "__main__":
 
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/states')
+def states():
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+# @app.route('/states/<int:id>')
+# def states_id(id):
+#     states = storage.all(id)
+#     return render_template('9-states.html', id=id)
+
+@app.route('/states/<id>')
+def states_id(id):
+    states = storage.all(State).values()
+    state = next((state for state in states if state.id == id), None)
+    if state is not None:
+        cities = sorted(state.cities, key=lambda city: city.name)
+    else:
+        cities = []
+    return render_template('9-states.html', state=state, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
